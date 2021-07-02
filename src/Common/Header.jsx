6f222e473cb3af0c5886625d84827df84285012b
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-07-03 04:32:02 +0530

    App created

---
+++ src/Common/Header.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useRef } from "react";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { SearchContext } from "../App";
+import "./styles/header.css";
+
+export const Header = () => {
+  const handleSearch = useContext(SearchContext);
+  const searchRef = useRef();
+  return (
+    <Navbar className="header-main" expand="lg">
+      <Navbar.Brand className="heading" href="/">
+        <b>Details Depictor</b>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="collapse">
+        <Nav className="mr-auto">
+          <Link
+            to="/create"
+            style={{
+              textDecoration: "none",
+              color: "black",
+              margin: "0px 20px",
+              cursor: "pointer",
+            }}
+          >
+            Create a post
+          </Link>
+          <Link
+            to="/liked"
+            style={{
+              textDecoration: "none",
+              color: "black",
+              margin: "0px 20px",
+              cursor: "pointer",
+            }}
+          >
+            Liked Posts
+          </Link>
+          <Link
+            to="/disliked"
+            style={{
+              textDecoration: "none",
+              color: "black",
+              margin: "0px 20px",
+              cursor: "pointer",
+            }}
+          >
+            Disliked Posts
+          </Link>
+          <Link
+            to="/CRUD_Application"
+            style={{
+              textDecoration: "none",
+              color: "black",
+              margin: "0px 20px",
+              cursor: "pointer",
+            }}
+          >
+            Home
+          </Link>
+        </Nav>
+        <Form className="form-search">
+          <FormControl
+            type="text"
+            ref={searchRef}
+            placeholder="Search Posts..."
+            className="mr-sm-2"
+            onChange={() => handleSearch(searchRef.current.value)}
+          />
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
---
+++ src/Components/Main/Body.jsx
@@ -0,0 +1,126 @@
+import React, { useContext, useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbDownAltIcon from "@material-ui/icons/ThumbDownAlt";
+import { ThumbDownAlt } from "@material-ui/icons";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import {
+  DeleteContext,
+  DislikedContext,
+  LikedContext,
+  PostsContext,
+  UpdateContext,
+  UserContext,
+} from "../../App";
+import "./styles/body.css";
+import { useHistory } from "react-router-dom";
+
+export const Body = ({ statusDisplay }) => {
+  const postList = useContext(PostsContext);
+  const userList = useContext(UserContext);
+  const handleDelete = useContext(DeleteContext);
+  const { updateName, updatePost, handleUpdate } = useContext(UpdateContext);
+  const { likedList, handleLiked } = useContext(LikedContext);
+  const { dislikedList, handleDisliked } = useContext(DislikedContext);
+
+  const history = useHistory();
+
+  const [moreClicked, setMoreClicked] = useState(false);
+
+  const handleMore = () => {
+    setMoreClicked(true);
+  };
+  const createPost = (data) => {
+    const changeLiked = (postId) => {
+      handleLiked(postId);
+    };
+    const changeDisliked = (postId) => {
+      handleDisliked(postId);
+    };
+    const getUpdate = (postId) => {
+      handleUpdate(postId);
+      history.push("/update");
+    };
+    return (
+      <>
+        <Card className="text-center m-4 w-75">
+          <Card.Header className="card-header bg-dark text-white">
+            <b>{data.title} </b>
+          </Card.Header>
+          <Card.Body>
+            {data.body} <hr />
+            <div className="d-flex justify-content-between align-items-center">
+              <b className="d-flex">
+                By{" "}
+                <div className="mx-2" style={{ textDecoration: "underline" }}>
+                  {userList.map((user) => {
+                    if (user.id == data.userId) {
+                      return user.name;
+                    }
+                  })}
+                </div>
+              </b>
+              <Button onClick={() => handleMore()}>MORE</Button>
+              {moreClicked && <div>More</div>}
+            </div>
+          </Card.Body>
+          <Card.Footer className="d-flex justify-content-between align-items-center">
+            <div className="d-flex">
+              <div
+                className={`${
+                  likedList.includes(data) ? "btn btn-success" : "btn btn-light"
+                } mx-2`}
+                onClick={() => changeLiked(data.id)}
+              >
+                <ThumbUpIcon />
+              </div>
+              <div
+                className={`${
+                  dislikedList.includes(data)
+                    ? "btn btn-danger"
+                    : "btn btn-light"
+                }`}
+                onClick={() => changeDisliked(data.id)}
+              >
+                <ThumbDownAlt />
+              </div>
+            </div>
+
+            <div className="d-flex">
+              <div
+                className="btn text-white d-flex align-items-center"
+                style={{ backgroundColor: "#5567ff" }}
+                onClick={() => getUpdate(data.id)}
+              >
+                <EditIcon />
+              </div>
+              <div
+                className="btn text-white d-flex align-items-center mx-2"
+                style={{ backgroundColor: "#ed0b4c" }}
+                onClick={() => handleDelete(data.id)}
+              >
+                <DeleteForeverIcon />
+              </div>
+            </div>
+          </Card.Footer>
+        </Card>
+      </>
+    );
+  };
+  return (
+    <div className="d-flex flex-column align-items-center">
+      {statusDisplay == "Disliked" ? (
+        <>{dislikedList.map(createPost)}</>
+      ) : (
+        <>
+          {statusDisplay == "Liked" ? (
+            <>{likedList.map(createPost)}</>
+          ) : (
+            <>{postList.map(createPost)}</>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
